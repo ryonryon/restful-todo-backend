bd6cf94e9a35c1efd9125debc1c10cf7e5e9eae4
ryotogashi <ryo.togashi.ca@gmail.com>
2019-10-29 14:56:30 -0700

    add user query.

--- src/controller/userTable.ts
+++ src/controller/userTable.ts
@@ -7,15 +7,10 @@ const userTableName = "user";
 class UserTable {
   static async createTableIfNotExists(): Promise<void> {
     const db = DBCommon.get();
+    const query = `CREATE TABLE IF NOT EXISTS ${userTableName} (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT )`;
     return new Promise((resolve, reject) => {
       try {
-        db.serialize(() => {
-          db.run(`CREATE TABLE IF NOT EXISTS ${userTableName} (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    name TEXT,
-                    email TEXT
-                  )`);
-        });
+        db.serialize(() => db.run(query));
         return resolve();
       } catch (err) {
         return reject(err);
@@ -25,8 +20,9 @@ class UserTable {
 
   static async count(): Promise<number> {
     const db = DBCommon.get();
+    const query = `SELECT (*) FROM ${userTableName}`;
     return new Promise((resolve, reject) => {
-      db.get(`SELECT (*) FROM ${userTableName}`, (err, row) => {
+      db.get(query, (err, row) => {
         if (err) return reject(err);
         return resolve(row["count(*)"]);
       });
@@ -35,14 +31,10 @@ class UserTable {
 
   static async createUser(name: string, email: string): Promise<void> {
     const db = DBCommon.get();
+    const query = `INSERT OR REPLACE INTO ${userTableName} (name, email) VALUES ($name, $email)`;
     return new Promise((resolve, reject) => {
       try {
-        db.get(
-          `INSERT OR REPLACE INTO ${userTableName}
-          (name, email) VALUES ($name, $email)`,
-          name,
-          email
-        );
+        db.get(query, [name, email]);
         return resolve();
       } catch (err) {
         return reject(err);
@@ -53,14 +45,40 @@ class UserTable {
   static async getUsers(): Promise<User[]> {
     const db = DBCommon.get();
     const users: User[] = [];
+    const query = `SELECT * FROM ${userTableName}`;
     return new Promise((resolve, reject) => {
-      db.all(`SELECT * FROM ${userTableName}`, (err, rows) => {
+      db.all(query, (err, rows) => {
         if (err) return reject(err);
         rows.forEach(row =>
           users.push(new User(row["id"], row["name"], row["email"]))
         );
         return resolve(users);
       });
+    });
+  }
+
+  static async getUser(name: string, email: string): Promise<User | null> {
+    const db = DBCommon.get();
+    const query = `SELECT * FROM ${userTableName} WHERE name = $name AND email = $email`;
+    return new Promise((resolve, reject) => {
+      db.get(query, [name, email], (err, row) => {
+        if (err) return reject(err);
+        if (row === null) return resolve(null);
+        return resolve(new User(row["id"], row["name"], row["email"]));
+      });
+    });
+  }
+
+  static async deleteUser(id: number): Promise<void> {
+    const db = DBCommon.get();
+    const query = `DELETE FROM ${userTableName} WHERE id = $id`;
+    return new Promise((resolve, reject) => {
+      try {
+        db.get(query, id);
+        return resolve();
+      } catch (err) {
+        return reject(err);
+      }
     });
   }
 }
